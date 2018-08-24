753987b32b5d4917bf28072245ebe8d50d1d38ce
silvestrevivo <silvestrevivomillan@gmail.com>
2018-08-25 00:02:08 +0200

    explanations about HOC functionality

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -30,3 +30,21 @@ export default ChildComponent => {
 
   return connect(mapStateToProps)(ComposedComponent);
 };
+
+/*
+This component works as a wrapper for CommentBox component:
+
+  1. With mapStateToProps we get the Boolean value of the user log
+
+  2. componentDidMount trigger the function shouldNavigateAway
+
+  3. shouldNavigateAway is going to make a conditional: if false
+    we are redirect to '/' path through 'react-router-dom', if true
+    we have access to CommentBox component and we are able to write
+    in the textarea
+
+  4. We pass all the props with the spread operator to the child
+
+  5. In CommentBox, we are wrapping the component by the export default
+
+*/
--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,6 +1,7 @@
 import { CHANGE_AUTH } from 'actions/types';
 
 export default function(state = false, action) {
+  // false, the user is not logged in => by default
   switch (action.type) {
     case CHANGE_AUTH:
       return action.payload;
